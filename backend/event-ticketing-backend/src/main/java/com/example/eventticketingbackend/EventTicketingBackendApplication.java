143034a0564a273e7ad319056b95ba46ab23043c
Yoonus Arshaan <yoonusarshaan@gmail.com>
2024-12-05 13:27:42 +0530

    "Initial commit: Set up Angular and Spring Boot project structure"

---
+++ backend/event-ticketing-backend/src/main/java/com/example/eventticketingbackend/EventTicketingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventticketingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingBackendApplication.class, args);
+	}
+
+}
---
+++ backend/event-ticketing-backend/src/test/java/com/example/eventticketingbackend/EventTicketingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventticketingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
